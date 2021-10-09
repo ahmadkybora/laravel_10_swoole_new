c5f1e3d34cba833260f61c4bf80b10cc710ddc7d
ahmadkybora <ahmadkybora@gmail.com>
2021-10-09 18:16:49 +0330

    new

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+
+
+<!doctype html>
+<html>
+
+<head>
+    <title>Real-Time with Swoole Laravel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <h1 class="text-center">این یک چت بلادرنگ ساده است</h1>
+    <ul id="messages" dir="rtl"></ul>
+    <form action="">
+        <input id="m" autocomplete="off" /><button>ارسال</button>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.0.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
+    <script src="https://unpkg.com/jalali-moment/dist/jalali-moment.browser.js"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
---
+++ routes/websocket.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use Illuminate\Http\Request;
+use SwooleTW\Http\Websocket\Facades\Websocket;
+
+/*
+|--------------------------------------------------------------------------
+| Websocket Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register websocket events for your application.
+|
+*/
+
+Websocket::on('connect', function ($websocket, Request $request) {
+    // called while socket on connect
+});
+
+Websocket::on('disconnect', function ($websocket) {
+    // called while socket on disconnect
+});
+
+Websocket::on('message', function ($websocket, $data) {
+    print_r($data)."\n";
+    $websocket->broadcast()->emit('send', $data);
+});
